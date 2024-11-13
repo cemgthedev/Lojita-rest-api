339da7941416046e253c97377925a9363b2c7f05
cemgthedev <cemg.the.dev@gmail.com>
2024-11-13 16:49:02 -0300

    feat: implementação da rota update de usuário
    implementação da rota de update passando id na rota e o user atualizado no body da requisição

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ def read_root():
     return {"message": "Seja bem vindo a Lojita"}
 
 # Rota para criar um novo usuário
-@app.post("/users/")
+@app.post("/users")
 async def create_user(user: User):
     try:
         # Escrever os dados no arquivo CSV
@@ -63,5 +63,39 @@ async def get_user(id: str):
                 if row["id"] == id:
                     return {"user": row};
             return {"message": "User not found"};
+    except Exception as e:
+        return {"error": str(e)}
+
+@app.put("/users/{id}")
+async def update_user(id: str, user: User):
+    try:
+        with open(path_directories["users"], mode="r", newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file);
+            rows = list(reader);
+            updated = False;
+            
+            for row in rows:
+                if row["id"] == id:
+                    row["name"] = user.name;
+                    row["year"] = user.year;
+                    row["cpf"] = user.cpf;
+                    row["gender"] = user.gender;
+                    row["phone_number"] = user.phone_number;
+                    row["address"] = user.address;
+                    row["email"] = user.email;
+                    row["password"] = user.password;
+                    
+                    updated = True;
+                    break;
+            
+            if updated:
+                with open(path_directories["users"], mode="w", newline="", encoding="utf-8") as file:
+                    writer = csv.DictWriter(file, fieldnames=headers["users"]);
+                    writer.writeheader();
+                    writer.writerows(rows);
+                    
+                return {"message": "User updated successfully", "user": user};
+            else:
+                return {"message": "User not found"};
     except Exception as e:
         return {"error": str(e)}
