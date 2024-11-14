602b9ec6f8767baae60157728c842f5c3394e052
cemgthedev <cemg.the.dev@gmail.com>
2024-11-14 15:12:41 -0300

    feat: implementação da rota de cálculo do hash 256 do csv de favoritos

--- src/services/favorites.py
+++ src/services/favorites.py
@@ -2,6 +2,7 @@ from models import Favorite
 from utils.generate_id import generate_id
 from services.configs import headers, path_directories
 import csv
+import hashlib
 
 from fastapi import APIRouter
 
@@ -104,5 +105,23 @@ async def get_quantity_favorites():
             rows = list(reader);
         
             return {"quantity": len(rows)};
+    except Exception as e:
+        return {"error": str(e)}
+    
+@router.get("/hash256/favorites")
+async def get_hash256_favorites():
+    try:
+        with open(path_directories["favorites"], mode="rb") as file:
+            hash256 = hashlib.sha256();
+            
+            # Tamanho de 100kb para calculo do hash
+            kbytes = 100 * 1024
+            
+            # Lendo o arquivo em blocos de 100kb para evitar o uso de muita memória e atualizando cálculo do hash
+            for block in iter(lambda: file.read(kbytes), b''):
+                hash256.update(block);
+            
+            # Retornando o hash 256 em formato hexadecimal
+            return {"hash256": hash256.hexdigest()};
     except Exception as e:
         return {"error": str(e)}
