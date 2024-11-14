3d4ba56c0ec213da4b6f9a1ffecdaabe44749802
cemgthedev <cemg.the.dev@gmail.com>
2024-11-14 15:47:42 -0300

    feat: implementação da rota de backup de produtos

--- src/services/products.py
+++ src/services/products.py
@@ -1,10 +1,12 @@
 from models import Product
 from utils.generate_id import generate_id
-from services.configs import headers, path_directories
+from services.configs import headers, file_names, path_directories
 import csv
 import hashlib
+import zipfile
 
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
@@ -146,5 +148,26 @@ async def get_hash256_products():
             
             # Retornando o hash 256 em formato hexadecimal
             return {"hash256": hash256.hexdigest()};
+    except Exception as e:
+        return {"error": str(e)}
+    
+@router.get("/backup/products")
+async def get_backup_products():
+    try:
+        # Define o nome do arquivo ZIP e o caminho
+        
+        zip_name = file_names["products"].replace(".csv", ".zip")
+        zip_path = path_directories["products"].replace(".csv", ".zip")
+        
+        # Cria o arquivo ZIP e adiciona o CSV nele
+        with zipfile.ZipFile(zip_path, "w") as zip_file:
+            zip_file.write(path_directories["products"], arcname=file_names["products"])
+
+        # Retorna o arquivo ZIP para download
+        return FileResponse(
+            path=zip_path,
+            filename=zip_name,
+            media_type="application/zip"
+        )
     except Exception as e:
         return {"error": str(e)}
